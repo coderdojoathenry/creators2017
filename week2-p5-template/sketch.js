bbdeba0e2bce2c6628aa3facbf7179579265ed55
Mark Davis <mark_davis2@hpe.com>
2017-10-02 09:31:09 +0100

    Added p5 template

---
+++ week2-p5-template/sketch.js
@@ -0,0 +1,16 @@
+function setup(){
+    createCanvas(400, 400);
+    background(0);
+
+    rect(200, 200, 100, 100);
+
+    fill(255, 0, 0);
+    ellipse(10, 200, 100, 100);
+
+    fill(255, 255, 0);    
+    ellipse(100, 200, 50, 100);    
+}
+
+function draw(){
+    
+}
