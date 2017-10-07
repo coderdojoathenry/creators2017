82d066fa742845ea7ae70f487dfe5b103286ac64
Mark Davis <mark_davis2@hpe.com>
2017-10-07 12:25:39 +0100

    Added week 3 initially

---
+++ week3-variables/sketch.js
@@ -0,0 +1,12 @@
+function setup(){
+    createCanvas(windowWidth, windowHeight);  
+    
+}
+
+function draw(){
+    background(0);
+    stroke(255);
+    fill(255);
+    textSize(50);
+    text("Hello, World", 100, 100);    
+}
