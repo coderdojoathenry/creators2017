c948e8b9e38ccbfe87f7ea18ebbce92d8f25d79a
Kieran Coughlan <KieranCoughlan@gmail.com>
2018-01-27 14:03:58 +0000

    Small tweak to speeds

--- week11-spirograph/sketch.js
+++ week11-spirograph/sketch.js
@@ -15,9 +15,9 @@ function draw(){
 
 function mouseClicked() {
     let r0 = random(10, 100);
-    let s0 = random(0.1, 5);
+    let s0 = random(0.1, 50);
     let r1 = random(10, 100);
-    let s1 = random(0.1, 5);
+    let s1 = random(0.1, 50);
     let colour = color(random(255), random(255), random(255));
     spiros.push(new Spirograph(createVector(mouseX, mouseY), r0, s0, r1, s1, colour))
 }
